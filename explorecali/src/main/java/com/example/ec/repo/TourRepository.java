bc458a2e35d113432b233ba616e93f1296c21b70
Clay Hsiao <claytinghuan@gmail.com>
2019-12-12 23:19:33 +0800

    feat: add 3-5 src code

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 /**
@@ -20,4 +21,28 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Integer
      * @return List of found tours.
      */
     Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
